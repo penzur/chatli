edbbe0b9a624289c5fa3e6c4629beb9b9760c18d
Elizar Pepino <jupenz@gmail.com>
2017-02-25 22:54:58 +0800

    Refactor a little

--- README.md
+++ README.md
@@ -1,5 +1,31 @@
-## CHATLI
+## chatli
 
-CLI based chat app powered by Node.JS
+
+Demo cli-based chat application written entirely in Node.JS
 
 ![demo](./chatli.gif)
+
+#### Requirements
+- Node v7+
+- NPM
+
+
+#### Setup
+
+0. Get the source code first `git clone https://github.com/elizar/chatli.git && cd chatli`
+
+0. Yarn is optional but it's faster than npm so `npm i -g yarn`
+
+0. `yarn` or `npm install` it's up to you.
+
+#### Try it!
+
+0. Run the **server** first `node server.js`
+
+0. Then open a new tab and run the **client** `node client.js`. **&nbsp;ENJOY!&nbsp;**
+
+&nbsp;
+--
+&nbsp;
+
+> Follow me: [@jupenz](https://twitter.com/jupenz)
--- server.js
+++ server.js
@@ -1,8 +1,39 @@
+/**
+Copyright (c) 2017 Elizar Pepino <jupenz@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
 'use strict';
 
 const sockets = [];
+const port = process.env.CHATLI_PORT || 31337;
 
-// Format message with nickname and JSON stringify for payload
+/**
+ * Format's the message to be sent to the clients
+ * from the server.
+ *
+ * @param nickname {String} The sender's nickname
+ * @param message {String} The message string
+ *
+ * @returns undefined
+ */
 const jsonStr = (nickname, message) => {
   return JSON.stringify({
     nickname,
@@ -10,7 +41,14 @@ const jsonStr = (nickname, message) => {
   });
 };
 
-// Broadcasts to a given sockets excluding an optional socket param
+/**
+ * Broadcasts message to a given list of sockets
+ *
+ * @param sockets {Array} A list of active/connected socket clients
+ * @param message {String} The message to be sent to the clients
+ *
+ * @returns undefined
+ */
 const broadcast = (sockets, message, excluded = undefined) => {
   sockets
     .forEach(s => {
@@ -19,92 +57,101 @@ const broadcast = (sockets, message, excluded = undefined) => {
     });
 };
 
+/**
+ * Handles disconnected client
+ *
+ * @param sock {Object} A socket client
+ *
+ * @returns undefined
+ */
+function socketOnEndHandler() {
+  const sock = this;
+  const index = sockets.indexOf(sock);
+
+  sockets.splice(index, 1);
+  if (sock.nickname) {
+    console.log(`[ Socket Server ] - user ${sock.nickname} disconnected!`);
+    broadcast(
+      sockets,
+      `${sock.nickname} disconnected from the server`
+    );
+  }
+};
+
+/**
+ * Handles message being sent from the clients. Also context of this function
+ * is bound to a socket client.
+ *
+ * @param data {Buffer} Data from the clients
+ *
+ * @returns undefined
+ */
+function socketOnDataHandler(data) {
+  const sock = this;
+  const message = data.toString().trim();
+
+  if(message.match(/^\/(quit|disconnect|exit|dc)/i)) {
+    sock.write('Goodbye!\n');
+    sock.end();
+    return;
+  }
+
+  if(message.match(/^\/nick /i)){
+    const nickname = message.split(' ')[1].trim();
+
+    if (nickname === '') {
+      sock.write('Please enter a valid nickname!\n');
+      return;
+    }
+
+    if (sockets.find(s => s.nickname === nickname)) {
+      sock.write('That nickname is already taken!\n');
+      return;
+    }
+
+    const oldNick = sock.nickname;
+    sock.nickname = nickname;
+
+    if (oldNick) {
+      sock.write('You are now known as ' + nickname);
+      return;
+    }
+
+    sockets.push(sock);
+
+
+    sock.write('Congrats! You are now registered as ' + nickname + '\n');
+    console.log(`[ Socket Server ] - user ${nickname} joined the server`);
+    broadcast(sockets, `${sock.nickname} joined`, sock);
+
+    clearTimeout(sock.timeout);
+    return;
+  }
+
+  if (sockets.indexOf(sock) !== -1) {
+    broadcast(sockets, jsonStr(sock.nickname, message));
+  }
+}
+
 require('net')
 
-  // Create socket server
-  // and register each socket with proper nickname identifier
+  // Create the socket server
   .createServer(sock => {
-    // Send greetings
     sock.write('Greetings! Please register with /nick <username>\n');
     sock.write('You have 20 seconds to register\n');
-    // Disconnect after 20 seconds if not yet registered
-    const timeout = setTimeout(() => {
+
+    // attach timeout to socket object so we can access it from event handlers
+    sock.timeout = setTimeout(() => {
       sock.write('You did not register\n');
       sock.write('Disconnecting...\n');
       sock.end();
     }, 1000 * 20);
 
-    sock.on('end', () => {
-      const i = sockets.indexOf(sock);
-      sockets.splice(i, 1);
-      if (sock.nickname) {
-        console.log(`[ Socket Server ] - user ${sock.nickname} disconnected!`);
-        // broadcast
-        broadcast(
-          sockets,
-          `${sock.nickname} disconnected from the server`
-        );
-      }
-    });
-
-    // On message
-    sock.on('data', d => {
-      const message = d.toString().trim();
-
-      if(message.match(/^\/(quit|disconnect|exit|dc)/i)) {
-        sock.write('Goodbye!\n');
-        sock.end();
-        return;
-      }
-
-      // Check for registration
-      if(message.match(/^\/nick /i)){
-        const nickname = message.split(' ')[1].trim();
-
-        // if empty
-        if (nickname === '') {
-          sock.write('Please enter a valid nickname!\n');
-          return;
-        }
-
-        // if nickname is taken
-        if (sockets.find(s => s.nickname === nickname)) {
-          sock.write('That nickname is already taken!\n');
-          return;
-        }
-
-        // Set or update nickname
-        const oldNick = sock.nickname;
-        sock.nickname = nickname;
-
-        // If registered already then do nothing. We just
-        // change the user's nickname
-        if (oldNick) {
-          sock.write('You are now known as ' + nickname);
-          return;
-        }
-
-        // Register
-        sockets.push(sock);
-
-
-        // Notify
-        sock.write('Congrats! You are now registered as ' + nickname + '\n');
-        console.log(`[ Socket Server ] - user ${nickname} joined the server`);
-        broadcast(sockets, `${sock.nickname} joined`, sock);
-
-        clearTimeout(timeout);
-        return;
-      }
-
-      // For regular messages
-      if (sockets.indexOf(sock) !== -1) {
-        broadcast(sockets, jsonStr(sock.nickname, message));
-      }
-    });
+    sock.on('end', socketOnEndHandler.bind(sock));
+    sock.on('data', socketOnDataHandler.bind(sock));
   })
 
-  // Bind 31337 port
-  .listen(31337, () => {
-    console.log('[ Socket Server ] - Now accepting connection on port 31337');
+  // Bind
+  .listen(port, () => {
+    console.log('[ Socket Server ] - Now accepting connection on port ' + port);
   });
